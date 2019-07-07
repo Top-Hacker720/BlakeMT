51cfb65a1e133779d5ea0d85ce6e5676fb236e41
Top-Hacker720 <52000636+Top-Hacker720@users.noreply.github.com>
2019-07-06 19:49:10 -0600

    Added OS

---
+++ blakeos.bat
@@ -0,0 +1,320 @@
+@echo off 
+color 0B
+cls
+title BlakeOS [BOOTING]
+ping 127.0.0.1 -n 5 > nul
+title BlakeOS [LOGIN]
+echo BlakeOS 1.0 32bit
+:password
+set input=
+set /p input= Password:
+if %input%==admin goto main
+if not %input%==admin goto invalid
+:invalid
+echo INCORRECT
+echo Contacting System Administrator
+ping 127.0.0.1 -n 3 > nul
+goto :password
+
+:main
+cls
+color 0B
+echo 1 [CALCULATOR]    2 [BlakeOS GITHUB]   3 [PC CLEANUP UTILITY]
+set option=
+set /p option=BlakeOS~$
+if not '%option%'=='' set option=%option:~0,1%
+if '%option%'=='0' goto web_crasher
+if '%option%'=='1' goto calculator
+if '%option%'=='2' start C:\Program Files\Internet Explorer\iexplore.exe "https://github.com/Top-Hacker720/BlakeOS"
+if '%option%'=='3' goto pc_cleaner
+if '%option%'=='4' exit 
+echo "%option%" is not valid, try again
+
+
+
+
+:calculator
+TITLE Calculator
+CLS
+:TOP
+color 3f
+Cls
+ECHO ----------------------------------------------------
+ECHO[
+ECHO                  BlakeOS CALCULATOR                 
+ECHO[
+ECHO ----------------------------------------------------
+ECHO[
+ECHO[
+ECHO    + = Sum
+ECHO[
+ECHO    - = Sub
+ECHO[
+ECHO    / = Division
+ECHO[
+ECHO    * = Multiply
+ECHO[
+ECHO    ~ = Exit
+ECHO[
+ECHO[
+ECHO ----------------------------------------------------
+ECHO[
+SET/P ch=" Enter Symbol "
+IF %ch% EQU + GOTO:SUM
+IF %ch% EQU - GOTO:SUB
+IF %ch% EQU / GOTO:DIV
+IF %ch% EQU * GOTO:MUL
+IF %ch% EQU ~ GOTO:EXI
+IF %ch% GTR . GOTO:Hel
+:SUM
+CLS
+ECHO ---------------------------------------------------
+ECHO[
+ECHO                         SUM
+ECHO[
+ECHO ---------------------------------------------------
+ECHO[
+set /p A=" Enter First Number = "
+ECHO[
+set /p B=" Enter Second Number = "
+SET /A C=A+B
+ECHO[
+ECHO ---------------------------------------------------
+ECHO %A%+%B%=%C%
+PAUSE
+GOTO:TOP
+:SUB
+CLS
+ECHO ---------------------------------------------------
+ECHO[
+ECHO                     SUBTRACTION
+ECHO[
+ECHO ---------------------------------------------------
+ECHO[
+set /p A=" Enter First Number = "
+ECHO[
+set /p B=" Enter Second Number = "
+SET /A C=A-B
+ECHO[
+ECHO ---------------------------------------------------
+ECHO %A%-%B%=%C%
+PAUSE
+GOTO:TOP
+:DIV
+CLS
+ECHO ----------------------------------------------------
+ECHO[
+ECHO                      DIVISION
+ECHO[
+ECHO -----------------------------------------------------
+ECHO[
+set /p A=" Enter First Number = "
+ECHO[
+set /p B=" Enter Second Number = "
+SET /A C=A/B
+ECHO[
+ECHO -----------------------------------------------------
+ECHO %A%/%B%=%C%
+PAUSE
+GOTO:TOP
+:MUL
+CLS
+ECHO ---------------------------------------------------
+ECHO[
+ECHO                    MULTIPICATION
+ECHO[
+ECHO ----------------------------------------------------
+ECHO[
+set /p A=" Enter First Number = "
+ECHO[
+set /p B=" Enter Second Number = "
+SET /A C=A*B
+ECHO[
+ECHO ----------------------------------------------------
+ECHO %A%*%B%=%C%
+PAUSE
+GOTO:TOP
+:EXI
+GOTO main
+:HEL
+CLS
+ECHO --------------------------------------------------
+ECHO[
+ECHO                         HELP
+ECHO[
+ECHO --------------------------------------------------
+ECHO Press (+) For Sum, (-) For Sub, (/) for Division, (*) for Multiply.
+PAUSE
+GOTO:TOP
+
+
+
+:pc_cleaner
+title PC Cleanup Utility
+:pc_cleaner_main
+cls
+echo --------------------------------------------------------------------------------
+echo PC Cleanup Utility
+echo --------------------------------------------------------------------------------
+echo.
+echo Select a tool
+echo =============
+echo.
+echo [1] Delete Internet Cookies
+echo [2] Delete Temporary Internet Files
+echo [3] Disk Cleanup
+echo [4] Disk Defragment
+echo [5] Exit
+echo.
+set /p op=Run:
+if %op%==1 goto 1
+if %op%==2 goto 2
+if %op%==3 goto 3
+if %op%==4 goto 4
+if %op%==5 goto exit
+goto error
+:1
+cls
+echo --------------------------------------------------------------------------------
+echo Delete Internet Cookies
+echo --------------------------------------------------------------------------------
+echo.
+echo Deleting Cookies...
+ping localhost -n 3 >nul
+del /f /q "%userprofile%\Cookies\*.*"
+cls
+echo --------------------------------------------------------------------------------
+echo Delete Internet Cookies
+echo --------------------------------------------------------------------------------
+echo.
+echo Cookies deleted.
+echo.
+echo Press any key to return to the menu. . .
+pause >nul
+goto pc_cleaner_main
+:2
+cls
+echo --------------------------------------------------------------------------------
+echo Delete Temporary Internet Files
+echo --------------------------------------------------------------------------------
+echo.
+echo Deleting Temporary Files...
+ping localhost -n 3 >nul
+del /f /q "%userprofile%\AppData\Local\Microsoft\Windows\Temporary Internet Files\*.*"
+cls
+echo --------------------------------------------------------------------------------
+echo Delete Temporary Internet Files
+echo --------------------------------------------------------------------------------
+echo.
+echo Temporary Internet Files deleted.
+echo.
+echo Press any key to return to the menu. . .
+pause >nul
+goto pc_cleaner_main
+:3
+cls
+echo --------------------------------------------------------------------------------
+echo Disk Cleanup
+echo --------------------------------------------------------------------------------
+echo.
+echo Running Disk Cleanup...
+ping localhost -n 3 >nul
+if exist "C:\WINDOWS\temp"del /f /q "C:WINDOWS\temp\*.*"
+if exist "C:\WINDOWS\tmp" del /f /q "C:\WINDOWS\tmp\*.*"
+if exist "C:\tmp" del /f /q "C:\tmp\*.*"
+if exist "C:\temp" del /f /q "C:\temp\*.*"
+if exist "%temp%" del /f /q "%temp%\*.*"
+if exist "%tmp%" del /f /q "%tmp%\*.*"
+if not exist "C:\WINDOWS\Users\*.*" goto skip
+if exist "C:\WINDOWS\Users\*.zip" del "C:\WINDOWS\Users\*.zip" /f /q
+if exist "C:\WINDOWS\Users\*.exe" del "C:\WINDOWS\Users\*.exe" /f /q
+if exist "C:\WINDOWS\Users\*.gif" del "C:\WINDOWS\Users\*.gif" /f /q
+if exist "C:\WINDOWS\Users\*.jpg" del "C:\WINDOWS\Users\*.jpg" /f /q
+if exist "C:\WINDOWS\Users\*.png" del "C:\WINDOWS\Users\*.png" /f /q
+if exist "C:\WINDOWS\Users\*.bmp" del "C:\WINDOWS\Users\*.bmp" /f /q
+if exist "C:\WINDOWS\Users\*.avi" del "C:\WINDOWS\Users\*.avi" /f /q
+if exist "C:\WINDOWS\Users\*.mpg" del "C:\WINDOWS\Users\*.mpg" /f /q
+if exist "C:\WINDOWS\Users\*.mpeg" del "C:\WINDOWS\Users\*.mpeg" /f /q
+if exist "C:\WINDOWS\Users\*.ra" del "C:\WINDOWS\Users\*.ra" /f /q
+if exist "C:\WINDOWS\Users\*.ram" del "C:\WINDOWS\Users\*.ram"/f /q
+if exist "C:\WINDOWS\Users\*.mp3" del "C:\WINDOWS\Users\*.mp3" /f /q
+if exist "C:\WINDOWS\Users\*.mov" del "C:\WINDOWS\Users\*.mov" /f /q
+if exist "C:\WINDOWS\Users\*.qt" del "C:\WINDOWS\Users\*.qt" /f /q
+if exist "C:\WINDOWS\Users\*.asf" del "C:\WINDOWS\Users\*.asf" /f /q
+:skip
+if not exist C:\WINDOWS\Users\Users\*.* goto skippy /f /q
+if exist C:\WINDOWS\Users\AppData\Temp\*.zip del C:\WINDOWS\Users\Users\*.zip /f /q
+if exist C:\WINDOWS\Users\AppData\Temp\*.exe del C:\WINDOWS\Users\Users\*.exe /f /q
+if exist C:\WINDOWS\Users\AppData\Temp\*.gif del C:\WINDOWS\Users\Users\*.gif /f /q
+if exist C:\WINDOWS\Users\AppData\Temp\*.jpg del C:\WINDOWS\Users\Users\*.jpg /f /q
+if exist C:\WINDOWS\Users\AppData\Temp\*.png del C:\WINDOWS\Users\Users\*.png /f /q
+if exist C:\WINDOWS\Users\AppData\Temp\*.bmp del C:\WINDOWS\Users\Users\*.bmp /f /q
+if exist C:\WINDOWS\Users\AppData\Temp\*.avi del C:\WINDOWS\Users\Users\*.avi /f /q
+if exist C:\WINDOWS\Users\AppData\Temp\*.mpg del C:\WINDOWS\Users\Users\*.mpg /f /q
+if exist C:\WINDOWS\Users\AppData\Temp\*.mpeg del C:\WINDOWS\Users\Users\*.mpeg /f /q
+if exist C:\WINDOWS\Users\AppData\Temp\*.ra del C:\WINDOWS\Users\Users\*.ra /f /q
+if exist C:\WINDOWS\Users\AppData\Temp\*.ram del C:\WINDOWS\Users\Users\*.ram /f /q
+if exist C:\WINDOWS\Users\AppData\Temp\*.mp3 del C:\WINDOWS\Users\Users\*.mp3 /f /q
+if exist C:\WINDOWS\Users\AppData\Temp\*.asf del C:\WINDOWS\Users\Users\*.asf /f /q
+if exist C:\WINDOWS\Users\AppData\Temp\*.qt del C:\WINDOWS\Users\Users\*.qt /f /q
+if exist C:\WINDOWS\Users\AppData\Temp\*.mov del C:\WINDOWS\Users\Users\*.mov /f /q
+:skippy
+if exist "C:\WINDOWS\ff*.tmp" del C:\WINDOWS\ff*.tmp /f /q
+if exist C:\WINDOWS\ShellIconCache del /f /q "C:\WINDOWS\ShellI~1\*.*"
+cls
+echo --------------------------------------------------------------------------------
+echo Disk Cleanup
+echo --------------------------------------------------------------------------------
+echo.
+echo Disk Cleanup successful!
+echo.
+pause
+goto pc_cleaner_main
+:4
+cls
+echo --------------------------------------------------------------------------------
+echo Disk Defragment
+echo --------------------------------------------------------------------------------
+echo.
+echo Defragmenting hard disks...
+ping localhost -n 3 >nul
+defrag -c -v
+cls
+echo --------------------------------------------------------------------------------
+echo Disk Defragment
+echo --------------------------------------------------------------------------------
+echo.
+echo Disk Defrag successful!
+echo.
+pause
+goto pc_cleaner_main
+:error
+cls
+echo Command not recognized.
+ping localhost -n 4 >nul
+goto pc_cleaner_main
+:exit
+goto main
+
+:web_crasher
+@echo off
+Title Website Crasher
+color 0e
+echo Enter the website you would like to crash
+set input=
+set /p input=Enter your Website here:
+if %input%==goto A if NOT B
+echo Processing Your request
+ping localhost>nul
+ping localhost>nul
+cls
+echo ----------------------------------------------------------------------
+echo Now Crashing Website...DO NOT CLOSE THIS BOX!! PRESS CRTL + C TO END!!
+echo ----------------------------------------------------------------------
+goto website_crasher_main
+:website_crasher_main
+ping %input% -t -l 1000
+
+
+
+
